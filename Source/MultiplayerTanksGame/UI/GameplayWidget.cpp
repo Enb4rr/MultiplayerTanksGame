d3c65a1a0a8132f669a330762e065931ac173721
Kiran Wood <kiranhonradowood@gmail.com>
2026-04-18 15:36:45 -0700

    Finish health function

--- Source/MultiplayerTanksGame/UI/GameplayWidget.cpp
+++ Source/MultiplayerTanksGame/UI/GameplayWidget.cpp
@@ -8,5 +8,8 @@
 void UGameplayWidget::OnHealthChanged(float NewHealth, float MaxHealth)
 {
 	float Percentage = NewHealth / MaxHealth;
+
+	// Set bar and text
 	HealthProgressBar->SetPercent(Percentage);
+	HealthTextBlock->SetText(FText::FromString(FString::Printf(TEXT("%d%%"), static_cast<int32>(Percentage * 100))));
 }
